b0548a366509e423f183a158ebdcba30f13bbf11
Caballero75 <43910549+Caballero75@users.noreply.github.com>
2021-03-17 15:46:27 -0300

    first commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  // const ChatScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: StreamBuilder(
+          stream: Firestore.instance
+              .collection("/chats/gtfjta1UXYDDCjCPDKF9/messages")
+              .snapshots(),
+          builder: (ctx, streamSnapshot) {
+            if (streamSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final documents = streamSnapshot.data.documents;
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (context, index) => Container(
+                color: Colors.deepOrangeAccent,
+                padding: EdgeInsets.all(8),
+                child: Text(documents[index]['text']),
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection("/chats/gtfjta1UXYDDCjCPDKF9/messages")
+              .add({'text': "teste"});
+        },
+      ),
+    );
+  }
+}
